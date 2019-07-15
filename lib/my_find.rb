05589dd91cf24bc9edd19c703400bdff5efde435
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-15 20:51:34 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,9 @@ def my_find(collection)
   i = 0 
   yield_returns = []
   while i < collection.length
-  yield(collection[i])
-  binding.pry
+  if yield(collection[i])
+    return collection[i]
+  end
   i += 1
   end
 end
